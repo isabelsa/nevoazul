9798fa17f2f96b02eec56f40854590b9c5cc24d0
Isabel Sá <25415758+isabelsoasa@users.noreply.github.com>
2019-10-23 13:17:21 +0100

    feat(404): creates initial implementation for 404

---
+++ src/components/NotFound/NotFound.css.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const NotFound = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  width: 100vw;
+  height: 100vh;
+`
+
+export const NotFoundContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 import SEO from '@/components/SEO/SEO'
+import NotFound from '@/components/NotFound/NotFound'
 
 const NotFoundPage = () => (
   <>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <NotFound />
   </>
 )
 
