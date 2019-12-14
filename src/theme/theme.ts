03d8039339cce6bc1d70ffcefa1bb7004136448f
Isabel Sá <25415758+isabelsoasa@users.noreply.github.com>
2019-12-14 16:53:33 +0000

    fix(cleanup): clean up stary files

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,5 +1,3 @@
-import { media } from './breakpoints'
-
 export default {
   colors: {
     brand: {
@@ -29,7 +27,6 @@ export default {
       default: '#DC7D67',
     },
   },
-  media,
   transitions: {
     ease: (time: string = '300ms') => `${time} ease`,
     cubic: (time: string = '500ms') => `${time} cubic-bezier(0.2, 1, 0.2, 1)`,
